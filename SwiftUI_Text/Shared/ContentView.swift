29c8c1e198dbd6b949335f573ed68c2944e9d22d
Youth <99013115+kimscastle@users.noreply.github.com>
2022-04-26 00:43:58 +0900

    Feat: SwiftUI_Text

--- SwiftUI_Text/Shared/ContentView.swift
+++ SwiftUI_Text/Shared/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, World!")
+           .font(.system(size: 50, weight: .light, design: .serif))
+           .foregroundColor(.pink)
+           .frame(width: 200, height: 200)
+           .background(Color.yellow)
+           .border(.red, width: 3)
     }
 }
 
