67f89601b7643e2c2a7b8c3a51acfacc9f344faa
Youth <99013115+kimscastle@users.noreply.github.com>
2022-04-26 00:41:28 +0900

    Add: SwiftUI_Text

---
+++ SwiftUI_Text/Shared/SwiftUI_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TextApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
