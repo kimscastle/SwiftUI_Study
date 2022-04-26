4e128bd346b2e7f73de222ec5b2c172d3736760c
Youth <99013115+kimscastle@users.noreply.github.com>
2022-04-26 18:12:56 +0900

    Add: SwiftUI_Image

---
+++ SwiftUI_Image/Shared/SwiftUI_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImageApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
