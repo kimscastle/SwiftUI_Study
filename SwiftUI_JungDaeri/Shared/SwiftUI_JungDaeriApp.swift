096bd3c84499d916860ddb817110a5ea2c5c4d50
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-09 16:33:45 +0900

    Add: 정대리스위프트강의파일

---
+++ SwiftUI_JungDaeri/Shared/SwiftUI_JungDaeriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JungDaeriApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JungDaeriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
