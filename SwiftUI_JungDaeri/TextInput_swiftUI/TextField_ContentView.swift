6d51975db83d986f9860e71f0b0b5366911eea56
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-20 23:57:17 +0900

    Feat: SwiftUi Textfield

--- SwiftUI_JungDaeri/TextInput_swiftUI/TextField_ContentView.swift
+++ SwiftUI_JungDaeri/TextInput_swiftUI/TextField_ContentView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct TextField_ContentView: View {
     
-    @State private var inputValue: String = ""
+    @State private var username: String = ""
+    @State private var password: String = ""
     
     var body: some View {
-        TextField("글자를 입력해주세요!", text: $inputValue)
+        
+        VStack{
+            HStack {
+                TextField("아이디를 입력해주세요", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                Button {
+                    self.username = ""
+                } label: {
+                    Image(systemName: "multiply.circle.fill")
+                        .foregroundColor(.secondary)
+                }
+
+            }
+            SecureField("비밀번호를 입력해주세요", text: $password)
+                .textFieldStyle(.roundedBorder)
+            Text("입력한비번 : \(password)")
+        }
+        .padding(.horizontal, 50)
     }
 }
 
