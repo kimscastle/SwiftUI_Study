8f0f035a0ec5a08eeccb3e158af3847de4483967
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-21 18:38:46 +0900

    Add: SwiftUI앱만들기(정대리) 18강_cocoapod라이브러리설치

---
+++ SwiftUI_JungDaeri/Pods/Target Support Files/Pods-SwiftUI_JungDaeri (iOS)/Pods-SwiftUI_JungDaeri (iOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftUI_JungDaeri__iOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftUI_JungDaeri__iOS_
+@end
---
+++ SwiftUI_JungDaeri/Popup_swiftUI(lib)/PopUpContentView.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpContentView.swift
+//  SwiftUI_JungDaeri (iOS)
+//
+//  Created by uiskim on 2022/05/21.
+//
+
+import SwiftUI
+
+struct PopUpContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopUpContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopUpContentView()
+    }
+}
