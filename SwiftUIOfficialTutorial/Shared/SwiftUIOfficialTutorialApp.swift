81f6aeb7767f39dbb94b28c2008797d9c6740d4b
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-22 16:33:36 +0900

    Add: SwiftUIOfficialTutorialFile

---
+++ SwiftUIOfficialTutorial/Shared/SwiftUIOfficialTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOfficialTutorialApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOfficialTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
