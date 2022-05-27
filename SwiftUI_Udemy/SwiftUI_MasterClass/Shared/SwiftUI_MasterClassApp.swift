a15c8b162ed15e05828e221bd14758344a3a1f66
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-27 23:22:37 +0900

    Add: SwiftUI_MasterClass file

---
+++ SwiftUI_Udemy/SwiftUI_MasterClass/Shared/SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MasterClassApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
