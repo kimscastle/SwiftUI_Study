93170671da9aed77ab866cf0eb3f88a7c123074e
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-27 23:21:36 +0900

    Add: SwiftUI_Tinder file

---
+++ SwiftUI_Udemy/SwiftUI_Tinder/Shared/SwiftUI_TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TinderApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
