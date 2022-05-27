92e3f2abf5eb9f736d91e48ef75dc41a0ffc938f
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-27 23:23:19 +0900

    Add: SwiftUI_DogInstagram

---
+++ SwiftUI_Udemy/SwiftUI_DogInstagram/Shared/SwiftUI_DogInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DogInstagramApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DogInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
