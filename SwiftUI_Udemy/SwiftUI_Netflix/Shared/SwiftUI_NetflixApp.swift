b3efb8593980f4f1e340a9a74a34e99420525760
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-27 23:20:41 +0900

    Add: SwiftUI_Udemy Folder & SwiftUI_Netflix file

---
+++ SwiftUI_Udemy/SwiftUI_Netflix/Shared/SwiftUI_NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NetflixApp.swift
+//  Shared
+//
+//  Created by uiskim on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
