6063ee52694b74adabad53a1d32015578ff94beb
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-28 20:17:12 +0900

    Feat:  Large Movie Preview (Part 1)

--- SwiftUI_Udemy/SwiftUI_Netflix/IOS/GlobalHelpers.swift
+++ SwiftUI_Udemy/SwiftUI_Netflix/IOS/GlobalHelpers.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!)
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!)
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301")!)
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/302")!)
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibla", thumbnailURL: URL(string: "https://picsum.photos/200/303")!)
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!)
+//cmd + control + option + F = 새로만든 요소들 전체에 넣어줌
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibla", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
 
 let exampleMoives: [Movie] = [exampleMovie1, exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6]
---
+++ SwiftUI_Udemy/SwiftUI_Netflix/IOS/TopMoviePreview.swift
@@ -0,0 +1,57 @@
+//
+//  TopMoviePreview.swift
+//  SwiftUI_Netflix
+//
+//  Created by uiskim on 2022/05/28.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct TopMoviePreview: View {
+    var movie: Movie
+    
+    func isCategoryLast(cat: String) -> Bool {
+        let catCount = movie.categories.count
+        
+        if let index = movie.categories.firstIndex(of: cat) {
+            if index + 1 == catCount {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                HStack {
+                    ForEach(movie.categories, id: \.self) { category in
+                        HStack {
+                            Text(category)
+                            if isCategoryLast(cat: category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 3))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
